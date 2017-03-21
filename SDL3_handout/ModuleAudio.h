8e29be6fde8d1721de0b433a7de3d9aed46169f1
Adria-F <adriafc@ttc.cat>
2017-03-21 08:44:24 +0100

    scrolling background + music

---
+++ SDL3_handout/ModuleAudio.h
@@ -0,0 +1,19 @@
+#ifndef __ModuleAudio_H__
+#define __ModuleAudio_H__
+
+#include "Module.h"
+#include "SDL_mixer\include\SDL_mixer.h"
+
+class ModuleAudio : public Module
+{
+public:
+	ModuleAudio();
+	virtual ~ModuleAudio();
+
+	bool Load(const char* path);
+	bool Init();		
+	bool CleanUp();
+public:
+	Mix_Music* music = nullptr;
+};
+#endif
