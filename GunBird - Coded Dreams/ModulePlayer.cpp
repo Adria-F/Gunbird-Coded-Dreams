0b9e0afb6c96f3367629df5e9650d5346648494f
Paxillop <paxillop@hotmail.es>
2017-04-02 14:16:01 +0200

    Marion movement animations
    Idle, right and left animations!

--- GunBird - Coded Dreams/ModulePlayer.cpp
+++ GunBird - Coded Dreams/ModulePlayer.cpp
@@ -13,9 +13,35 @@ ModulePlayer::ModulePlayer()
 {
 
 	// idle animation (arcade sprite sheet)
-	idle.PushBack({68, 64, 18, 32}); //w - h - w - h
-	idle.speed = 0.2f;
+	idle.PushBack({ 5, 64, 21, 32 });//x - y - w - h
+	idle.PushBack({ 37, 64, 21, 32 });
+	idle.PushBack({ 68, 64, 21, 32 });
+	idle.PushBack({ 101, 64, 21, 32 });
+	idle.speed = 0.1f;
 
+	//right animation
+	//1st fase
+	right.PushBack({ 5, 129, 23, 32 });//x - y - w - h
+	right.PushBack({ 37, 129, 23, 32 });
+	right.PushBack({ 68, 129, 23, 32 });
+	right.PushBack({ 101, 129, 23, 32 });
+	//2nd fase
+	right.PushBack({ 5, 97, 23, 32 });
+	right.PushBack({ 37, 97, 23, 32 });
+	right.PushBack({ 68, 97, 23, 32 });
+	right.PushBack({ 101, 97, 23, 32 });
+	right.speed = 0.1f;
+
+	//left animation
+	left.PushBack({ 3, 33, 23, 32 });//x - y - w - h
+	left.PushBack({ 35, 33, 23, 32 });
+	left.PushBack({ 66, 33, 23, 32 });
+	left.PushBack({ 99, 33, 23, 32 });
+	left.PushBack({ 3, 0, 23, 32 });
+	left.PushBack({ 35, 0, 23, 32 });
+	left.PushBack({ 66, 0, 23, 32 });
+	left.PushBack({ 99, 0, 23, 32 });
+	left.speed = 0.1f;
 }
 
 ModulePlayer::~ModulePlayer()
@@ -55,6 +81,7 @@ update_status ModulePlayer::Update()
 	}
 	if (App->input->keyboard[SDL_SCANCODE_A] && position.x > 0)
 	{
+		current_animation = &left;
 		position.x -= speed;
 	}
 	if (App->input->keyboard[SDL_SCANCODE_S] && position.y < SCREEN_HEIGHT)
@@ -63,8 +90,16 @@ update_status ModulePlayer::Update()
 	}
 	if (App->input->keyboard[SDL_SCANCODE_D] && position.x < SCREEN_WIDTH - 18)
 	{
+		current_animation = &right;
 		position.x += speed;
 	}
+	/*
+	if (App->input->keyboard[SDL_SCANCODE_D] == 1)
+	{
+		current_animation = &forward;
+		position.x += speed;
+	}
+	*/
 	if (App->input->keyboard[SDL_SCANCODE_F] == KEY_STATE::KEY_DOWN) //The key state down is to control the spree you can shoot the bullets.
 	{
 		App->particles->AddParticle(App->particles->bullet, position.x + 5, position.y - 45);
