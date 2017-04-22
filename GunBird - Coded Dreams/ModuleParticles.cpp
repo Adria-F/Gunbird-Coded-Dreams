c2267f4f67e27156318c791678e851dde5a85a89
joseppi <josep.pi.serra@gmail.com>
2017-04-22 20:07:02 +0200

    Update to particles

--- GunBird - Coded Dreams/ModuleParticles.cpp
+++ GunBird - Coded Dreams/ModuleParticles.cpp
@@ -84,7 +84,7 @@ bool ModuleParticles::Start()
 	explosions_particle.anim.PushBack({ 322, 110, 99, 101 });
 	explosions_particle.life = 10000;
 	explosions_particle.speed.y = App->render->cam_speed;
-	explosions_particle.anim.loop = true;
+	explosions_particle.anim.loop = false;
 	explosions_particle.anim.speed = 1.0f;
 	
 
