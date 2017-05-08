843013d12f98c5f31ab1b5fc90885800b0d8fa7f
joseppi <josep.pi.serra@gmail.com>
2017-05-08 19:44:08 +0200

    Added tump.cpp.h

--- GunBird - CodedDreams/Enemy_Trump.h
+++ GunBird - CodedDreams/Enemy_Trump.h
@@ -17,6 +17,26 @@ public:
 
 	Path path;
 	Animation anim;
+	//pilot
+	Animation anim_pilot_confused;
+	Animation anim_pilot_left;
+	Animation anim_pilot_right;
+	Animation anim_pilot_idle;
+	Animation anim_pilot_turning;
+	Animation anim_pilot_up;
+
+	//punk
+	Animation anim_boy_confused;
+	Animation anim_boy_left;
+	Animation anim_boy_right;
+	Animation anim_boy_idle;
+	Animation anim_boy_turning;
+	Animation anim_boy_up;
+
+	//girl
+	Animation anim_girl_idle;
+	Animation anim_girl_turning;
+	Animation anim_girl_up;
 
 
 };
