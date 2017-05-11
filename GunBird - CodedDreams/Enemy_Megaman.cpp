e793a4fdabacb0435376b5ae02ca88a4fa94fe5e
joseppi <josep.pi.serra@gmail.com>
2017-05-11 10:20:21 +0200

    easter egg update

--- GunBird - CodedDreams/Enemy_Megaman.cpp
+++ GunBird - CodedDreams/Enemy_Megaman.cpp
@@ -18,12 +18,14 @@ Enemy_Megaman::Enemy_Megaman(int x, int y): Enemy(x, y)
 	idle.loop = true;
 
 	//Set path
-	path.PushBack({0.0f, 0.2f}, 100, &idle); //Si esta quiet en un punt ha de tenir velocitat y = 0.2 per moures a la mateixa velocitat que l'overlay
+	path.PushBack({0.0f, 0.09f}, 100, &idle); //Si esta quiet en un punt ha de tenir velocitat y = 0.2 per moures a la mateixa velocitat que l'overlay
 
 	//Set lives, initial_hp, points adn extra_anim
 	lives = 1;
 	initial_hp = lives;
 	extra_anim = false;
+
+	collider = App->collision->AddCollider({ x, y, 0, 0 }, COLLIDER_NONE, (Module*)App->enemies);
 }
 
 Enemy_Megaman::~Enemy_Megaman()
@@ -34,4 +36,5 @@ Enemy_Megaman::~Enemy_Megaman()
 void Enemy_Megaman::Move()
 {
 	position = original_pos + path.GetCurrentPosition(&animation);
+	lower_level = true;
 }
