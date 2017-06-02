c6ac7606fe7a4cd48e494b8861c6264a316f2e59
joseppi <josep.pi.serra@gmail.com>
2017-06-02 19:24:36 +0200

    Solved bug lower cannons shooting wrong from right

--- GunBird - CodedDreams/Enemy_RightSide.cpp
+++ GunBird - CodedDreams/Enemy_RightSide.cpp
@@ -56,7 +56,7 @@ Enemy_RightSide::~Enemy_RightSide()
 void Enemy_RightSide::Move()
 {
 	position = original_pos + path.GetCurrentPosition(&animation);
-	if (App->render->camera.y >= -2146) //-2146
+	if (App->render->camera.y >= -2077) //-2146
 	{
 		Shot_now = SDL_GetTicks() - Shot_Start_time;
 		if (Shot_now > Shot_Total_time || state >= 1) //Initial time
@@ -65,13 +65,13 @@ void Enemy_RightSide::Move()
 			if (Shot_now > Shot_Total_time1 && state == 2) //First Shoot
 			{
 				Shot_Start_time = SDL_GetTicks();
-				App->particles->AddParticle(App->particles->big_shot_particle, particle_type::P_BIG_SHOT, position.x + 15, position.y + App->render->camera.y + 60, COLLIDER_ENEMY_SHOT, 0, 270, ANGLE);
+				App->particles->AddParticle(App->particles->big_shot_particle, particle_type::P_BIG_SHOT, position.x + 31, position.y + App->render->camera.y + 60, COLLIDER_ENEMY_SHOT, 0, 270, ANGLE);
 				state = 1;
 			}
 			else if (Shot_now > Shot_Total_time2 && state == 1) //Second Shoot
 			{
 				Shot_Start_time = SDL_GetTicks();
-				App->particles->AddParticle(App->particles->big_shot_particle, particle_type::P_BIG_SHOT, position.x + 15, position.y + App->render->camera.y + 60, COLLIDER_ENEMY_SHOT, 0, 270, ANGLE);
+				App->particles->AddParticle(App->particles->big_shot_particle, particle_type::P_BIG_SHOT, position.x + 31, position.y + App->render->camera.y + 60, COLLIDER_ENEMY_SHOT, 0, 270, ANGLE);
 				state = 2;
 			}
 			else if (state == 3)
